afa9f68d81c2aa5a03b3bc041a3ebd5f6586f4b2
unknown <Kysluss@aol.com>
2018-08-26 17:34:08 -0400

    Moved actions.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import counterReducer from './store/reducers/counter';
@@ -14,7 +14,25 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = (store) => {
+    return next => {
+        return action => {
+            //Here is where we can do something
+            console.log('[Middleware] Dispatching', action);
+            //You can make changes to the action if you want here
+            //Executing next will pass the action along to the reducers
+            const result = next(action);
+            //We could do something with the result if we want
+            console.log('[Middleware] next state', store.getState());
+            //Always return the result
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
